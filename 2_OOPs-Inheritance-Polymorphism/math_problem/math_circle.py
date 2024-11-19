cd46a2e4195b35c4a6e14bac16aa47ca7d287030
Tutoo Joe Philip <tutoojoephilip@gmail.com>
2024-11-19 22:08:22 +0530

    Inheritance- area of circle problem

--- 2_OOPs-Inheritance-Polymorphism/math_problem/math_circle.py
+++ 2_OOPs-Inheritance-Polymorphism/math_problem/math_circle.py
@@ -5,6 +5,5 @@ class Math:
     @staticmethod
     def getCircleArea(radius):
         # note: in order to use the class variable, we have to mention the class_name.variable name format
-
         area = Math.PI * radius * radius
         return area
